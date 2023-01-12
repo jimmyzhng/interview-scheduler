8e87632899f7e12a55aa6b064522dc9393cab453
Jimmy <jimmyzhang1@hotmail.com>
2023-01-11 19:37:03 -0800

    Build the DayList

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,2 +1,26 @@
 import DayListItem from "./DayListItem";
+import React from "react";
 
+export default function DayList(props) {
+
+  // DayList receives days (array with id, name, spots)
+  // we need to convert that to an array of DayListItem components
+
+  // DayListItem component needs a key, name, spots, setDay (to change state), 
+  // and selected - this needs to know if the given parent day (props.day) = current item's name
+  const dayListArr = props.days.map(day => {
+    return (<DayListItem
+      key={day.id}
+      name={day.name}
+      spots={day.spots}
+      selected={day.name === props.day}
+      setDay={props.setDay}
+    />);
+  });
+
+  return (
+    <ul>
+      {dayListArr}
+    </ul>
+  );
+};
