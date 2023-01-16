d7f437d4aa710e61563489129f56b66c4c03a029
Jimmy <jimmyzhang1@hotmail.com>
2023-01-15 21:34:37 -0800

    With our plan of internal navigation, change our Appointment index to conditionally render our components. Implement this logic with our useVisualMode hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,14 +3,41 @@ import React, { Fragment } from "react";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode";
+import Form from "./Form";
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <Header time={props.time}></Header>
-      {props.interview ?
-        <Show student={props.interview.student} interviewer={props.interview.interviewer} /> :
-        <Empty />}
+      {mode === EMPTY && <Empty
+        onAdd={() => transition(CREATE)}
+      />
+      }
+      {mode === SHOW &&
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      }
+
+      {mode === CREATE &&
+        <Form
+          student={props.student}
+          interviewer={props.interviewer}
+          interviewers={[]}
+          onSave={props.onSave}
+          onCancel={() => back()}
+        />
+      }
     </article>
   );
 
