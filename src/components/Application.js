5d1e58aca29b3c53ee3a91288e40c2e23d1b69cb
Jimmy <jimmyzhang1@hotmail.com>
2023-01-15 17:46:05 -0800

    Add interviewers state, and associated tests with the helper function getInterview. This new helper function helps us change our interview object into one that can be used as a prop.

--- src/components/Application.js
+++ src/components/Application.js
@@ -11,34 +11,41 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   // Use selectors helper function to get appointments based on current state of day
   const dailyAppointments = getAppointmentsForDay(state, state.day);
+  console.log('daily appointments', dailyAppointments);
 
   // Selector function that takes in our appointments, and creates an array that we display
   const appointmentsArr = dailyAppointments.map(app => {
-    return <Appointment key={app.id} {...app} />;
+    // We need to transform the interview before passing it as a prop, since the data we 
+    // retrieved from the api is an object of objects.
+    // const interview = getInterview(state, app.interview);
+
+    return <Appointment key={app.id} id={app.id} time={app.time} interview={interview} />;
   });
+  console.log('');
 
   const setDay = day => setState({ ...state, day });
   // const setDays = days => setState(prev => ({ ...prev, days }));
 
   // Getting the data from scheduler-api with axios
-  // Promise.all resolves all promises, and returns aray of resolved values matching
+  // Promise.all resolves all promises, and returns array of resolved values matching
   // the order of the array passed to it
   useEffect(() => {
 
     Promise.all([
       Axios.get('/api/days'),
-      Axios.get('/api/appointments')
+      Axios.get('/api/appointments'),
+      Axios.get('/api/interviewers')
     ])
       // Our res is an array of the response received: [{days}, {appts}]
       .then(res => {
-        console.log('res.data[0]', res[0].data);
-        console.log('res[1]', res[1]);
-        setState(prev => ({ ...prev, days: res[0].data, appointments: res[1].data }));
+        console.log(res[2].data);
+        setState(prev => ({ ...prev, days: res[0].data, appointments: res[1].data, interviewers: res[2].data }));
       });
   }, []);
 
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -17,3 +17,18 @@ export function getAppointmentsForDay(state, day) {
   }
   return apptsArray;
 }
+
+export function getInterview(state, interview) {
+  // return a new object containing interview data (with interviewer info) that we can use as a prop
+  // It is needed because the prop requires the data in a specific way - where the interviewer key 
+  // has the interviewer's info object as the value
+  // It is currently only displaying the interviewer's id (1 in the example)
+
+  if (interview) {
+    let interviewerId = interview.interviewer;
+    interview.interviewer = state.interviewers[interviewerId];
+    return interview;
+  }
+
+  return null;
+}
